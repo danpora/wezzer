78f79ab0164b3ab2593a3e05edf8b7af25011bd3
danpora <danpora@post.bgu.ac.il>
2019-02-10 00:44:45 +0200

    add toRadians func to utils

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,19 @@
 const R = 6371e3; // earth radius in metres
 
 export function distance(lat1, lon1, lat2, lon2) {
-  const φ1 = lat1.toRadians();
-  const φ2 = lat2.toRadians();
-  const Δφ = (lat2-lat1).toRadians();
-  const Δλ = (lon2-lon1).toRadians();
+  const φ1 = toRadians(lat1);
+  const φ2 = toRadians(lat2);
+  const Δφ = toRadians((lat2-lat1));
+  const Δλ = toRadians((lon2-lon1));
   
   const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
   
   return R * c;
+}
+
+function toRadians(deg) {
+  return deg * ( Math.PI / 180 );
 }
