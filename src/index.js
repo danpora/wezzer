cb8f2aaf295a0ac9276f49262580a4382fa69d25
danpora <danpora@post.bgu.ac.il>
2019-02-17 23:56:28 +0200

    move to react

--- src/index.js
+++ src/index.js
@@ -1,54 +1,62 @@
-import { h, render, Component } from 'preact';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/SearchBar';
+
 import * as Utils from './utils';
 
 import './style.css';
-class App extends Component {
-    constructor () {
-        super();
+class App extends React.Component {
+  constructor() {
+    super();
 
-        this.state = {
-            defaultWeather: {},
-            reports: {},
-            myLocation: {
-                lat: 0,
-                lon: 0
-            }
-        }
-    }
+    this.state = {
+      defaultWeather: {},
+      reports: {},
+      myLocation: {
+        lat: 0,
+        lon: 0,
+      },
+    };
+  }
 
-    componentDidMount () {
-        navigator.geolocation.getCurrentPosition(
-            // user allowed position
-            (currentPos) => {
-                this.setState({
-                    myLocation: {
-                        lat: currentPos.coords.latitude,
-                        lon: currentPos.coords.longitude,
-                    }
-                })
-            }, 
-            // user denied position
-            (error) => {
-                // TODO:: alert to user that he'll have to pick location alone
-            }
-        );
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      // user allowed position
+      (currentPos) => {
+        const { latitude, longitude } = currentPos.coords;
 
-        fetch('https://0brc1jr0z3.execute-api.eu-west-1.amazonaws.com/v1/weather')
-            .then(r => r.json())
-            .then(r => {                
-                this.setState({ reports: r })
-            });
-    }
+        this.setState({
+          myLocation: {
+            lat: latitude,
+            lon: longitude,
+          },
+        });
 
-    render(props, state) {        
-        // return <span className="wrapper">{ parseInt(Utils.distance(state.myLocation.lat, state.myLocation.lon, 40.78788, -74.014313)) } meters from new york</span>;
-        return (
-            <div>
-                <span>{JSON.stringify(state.reports)}</span>
-            </div>
-        );
-    }
+        fetch(
+          `https://0brc1jr0z3.execute-api.eu-west-1.amazonaws.com/v1/weather?lon=${longitude}&lat=${latitude}`,
+        )
+          .then((r) => r.json())
+          .then((r) => {
+            this.setState({ reports: r });
+          });
+      },
+      // user denied position
+      (error) => {
+        // TODO:: alert to user that he'll have to pick location alone
+      },
+    );
+  }
+
+  render() {
+    // return <span className="wrapper">{ parseInt(Utils.distance(state.myLocation.lat, state.myLocation.lon, 40.78788, -74.014313)) } meters from new york</span>;
+    return (
+      <div>
+        <SearchBar />
+        <span>{JSON.stringify(this.state.reports)}</span>
+      </div>
+    );
+  }
 }
- 
-// render an instance of Clock into <body>:
-render(<App />, document.body);
+
+ReactDOM.render(<App />, document.getElementById('app'));
