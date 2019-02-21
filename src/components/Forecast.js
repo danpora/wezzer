9f0eb4a50b871d80f34769924a11142dac2532ac
danpora <danpora@post.bgu.ac.il>
2019-02-21 23:30:31 +0200

    move weather icon component to dedicated file

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import WeatherIcon from './WeatherIcon';
 
 export default function Forecast(props) {
   const { weather = [{}], name, sys = {}, main = {} } = props.data;
+
   return (
     <div style={{ padding: '15px 0' }}>
       <div style={{ textAlign: 'center' }}>
@@ -18,29 +19,3 @@ export default function Forecast(props) {
     </div>
   );
 }
-
-function WeatherIcon({ code }) {
-  const iconName = weatherCodeToIcon[code] || weatherCodeToIcon.default;
-
-  return <FontAwesomeIcon icon={iconName} size={'5x'} color="grey" />;
-}
-
-const weatherCodeToIcon = {
-  800: 'sun',
-  801: 'cloud-sun',
-  802: 'cloud',
-  803: 'cloud',
-  804: 'cloud',
-  500: 'cloud-rain',
-  500: 'cloud-showers-heavy',
-  501: 'cloud-showers-heavy',
-  502: 'cloud-showers-heavy',
-  503: 'cloud-showers-heavy',
-  504: 'cloud-showers-heavy',
-  511: 'cloud-showers-heavy',
-  520: 'cloud-showers-heavy',
-  521: 'cloud-showers-heavy',
-  522: 'cloud-showers-heavy',
-  531: 'cloud-showers-heavy',
-  default: 'meteor',
-};
