caa1f099f0f89ff7ca6abf0cfa6b7d0600ac2f66
danpora <danpora@post.bgu.ac.il>
2019-02-22 00:29:05 +0200

    extend reports component

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import * as Utils from '../utils';
 
-export default function Reports (props) {
+import WeatherIcon from './WeatherIcon';
+
+export default function Reports(props) {
+  const sortedReportsCounts = Utils.getDominantWeatherCodeFromReports(
+    props.data,
+  );
+  const isNoReports = sortedReportsCounts.length === 0;
+
   return (
-    <div>{JSON.stringify(Utils.getDominantWeatherCodeFromReports(props.data))}</div>
+    <div>
+      {isNoReports ? (
+        'No reports Available'
+      ) : (
+        <ReportsViewer reportsCounts={sortedReportsCounts} />
+      )}
+    </div>
+  );
+}
+
+function ReportsViewer (props) {
+  const [weatherCode, weatherCodeVoters] = props.reportsCounts[0];
+
+  return (
+    <React.Fragment>
+      <div style={{ padding: '15px 0' }}>
+        <div style={{ textAlign: 'center' }}>
+        <WeatherIcon code={weatherCode} />
+        </div>
+        <div style={{ textAlign: 'center', fontSize: '3em' }}>
+          {Utils.weatherCodeToDescription[weatherCode]}
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          {`According to ${weatherCodeVoters} humans (10Km around you)`}
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
