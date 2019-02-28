9cff148d282e3cedf273ef86ad9409dd232ea2ae
danpora <danpora@post.bgu.ac.il>
2019-03-01 00:08:05 +0200

    create weather code to description dynamicaly from weather options

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { weatherConditions } from './constants';
+
 const R = 6371e3; // earth radius in metres
 
 export function distance(lat1, lon1, lat2, lon2) {
@@ -36,21 +38,7 @@ export function getDominantWeatherCodeFromReports(reports) {
   return weatherCodeCounterSortedList;
 }
 
-export const weatherCodeToDescription = {
-  800: 'clear sky',
-  801: 'few clouds',
-  802: 'scattered clouds',
-  803: 'broken clouds',
-  804: 'overcast clouds',
-  500: 'light rain',
-  501: 'moderate rain',
-  502: 'heavy intensity rain',
-  503: 'very heavy rain',
-  504: 'extreme rain',
-  511: 'freezing rain',
-  520: 'light intensity shower rain',
-  521: 'shower rain',
-  522: 'heavy intensity shower rain',
-  531: 'ragged shower rain',
-  default: 'meteor',
-};
+export const weatherCodeToDescription = weatherConditions.reduce((acc, w) => ({
+  ...acc,
+  [w.code]: w.displayName
+}),{})
