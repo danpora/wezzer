07e8df4a2a96ab967e0bb43a38eebbc870e68ef3
danpora <danpora@post.bgu.ac.il>
2019-03-01 00:08:29 +0200

    define weatherCodeToIcon dynamical from weather options

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { weatherConditions } from '../constants';
 
-const weatherCodeToIcon = {
-  800: 'sun',
-  801: 'cloud-sun',
-  802: 'cloud',
-  803: 'cloud',
-  804: 'cloud',
-  500: 'cloud-rain',
-  500: 'cloud-showers-heavy',
-  501: 'cloud-showers-heavy',
-  502: 'cloud-showers-heavy',
-  503: 'cloud-showers-heavy',
-  504: 'cloud-showers-heavy',
-  511: 'cloud-showers-heavy',
-  520: 'cloud-showers-heavy',
-  521: 'cloud-showers-heavy',
-  522: 'cloud-showers-heavy',
-  531: 'cloud-showers-heavy',
-  default: 'meteor',
-};
+const weatherCodeToIcon = weatherConditions.reduce((acc, w) => ({
+  ...acc,
+  [w.code]: w.icon.day
+}), {})
 
 export default function WeatherIcon({ code }) {
 
