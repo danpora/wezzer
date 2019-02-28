a781daea11d0877774e1e38fdf34ef9b511cabd4
danpora <danpora@post.bgu.ac.il>
2019-02-28 23:31:06 +0200

    extend weather conditions definitions

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,48 @@
 export const weatherConditions = [
   {
     displayName: 'Clear Sky',
-    code: 800
+    code: 800,
+    icon: { day: 'sun', night: 'moon' },
+    mode: 'day'
   },
   {
     displayName: 'Few Clouds',
-    code: 801
+    code: 801,
+    icon: { day: 'cloud-sun', night: 'cloud-moon' },
+  },
+  {
+    displayName: 'Scattered Clouds',
+    code: 802,
+    icon: { day: 'cloud', night: 'cloud' },
+  },
+  {
+    displayName: 'Light Rain',
+    code: 500,
+    icon: { day: 'cloud-rain', night: 'cloud-moon-rain' },
+  },
+  {
+    displayName: 'Heavy Intensity Rain',
+    code: 521,
+    icon: { day: 'cloud-showers-heavy', night: 'cloud-showers-heavy' },
+  },
+  {
+    displayName: 'Moderate Rain',
+    code: 502,
+    icon: { day: 'cloud-sun-rain', night: 'cloud-moon-rain' },
+  },
+  {
+    displayName: 'Snow',
+    code: 601,
+    icon: { day: 'cloud-meatball', night: 'cloud-meatball' },
+  },
+  {
+    displayName: 'Fog',
+    code: 741,
+    icon: { day: 'smog', night: 'smog' },
+  },
+  {
+    displayName: 'Thunderstorm',
+    code: 211,
+    icon: { day: 'bolt', night: 'bolt' },
   }
-]
+];
