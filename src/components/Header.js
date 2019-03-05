31e9e2da313e56332d543294a346542dca0216ef
danpora <danpora@post.bgu.ac.il>
2019-03-05 19:34:19 +0200

    Header component | clean unused imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
 import CloudIcon from '@material-ui/icons/Cloud';
-import LocationCityIcon from '@material-ui/icons/LocationCity';
-import SearchIcon from '@material-ui/icons/Search';
+
 
 const styles = {
   root: {
