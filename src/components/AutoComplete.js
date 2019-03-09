0e9e0a9a1daefdb6a956c83ecef8814f3ee1b42b
danpora <danpora@post.bgu.ac.il>
2019-03-09 23:07:44 +0200

    extend autocomplete configs,  implement selection query

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -13,16 +13,25 @@ export default class AsyncExample extends React.Component {
       options: []
     };
 
-    this._handleSearch = this._handleSearch.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
   }
 
-  makeAndHandleRequest() {
+  makeAndHandleRequest(query) {
     return new Promise((resolve, reject) => {
-      resolve(["london"]);
+      fetch(`https://0brc1jr0z3.execute-api.eu-west-1.amazonaws.com/v1/cities?name=${query}`)
+        .then(r => r.json())
+        .then(result => {
+          const cities = result.map(c => ({ name: c.name, location: c.coord }));
+          resolve(cities);
+        })
+        .catch(err => {
+          reject(err);
+        })
     });
   }
 
-  _handleSearch(query) {
+  handleSearch(query) {
     this.setState({ isLoading: true });
     this.makeAndHandleRequest(query).then(options => {
       this.setState({
@@ -32,14 +41,29 @@ export default class AsyncExample extends React.Component {
     });
   }
 
+  handleSelection (query) {
+    if (query.length === 0) return;
+
+    const selection = query[0];
+    this.props.handleSelection(selection);
+  }
+
   render() {
     return (
       <Fragment>
         <AsyncTypeahead
           {...this.state}
+          useCache
           id="async-typeahead"
-          // minLength={3}
-          onSearch={this._handleSearch}
+          labelKey="name"
+          minLength={2}
+          maxResults={30}
+          onSearch={this.handleSearch}
+          onChange={this.handleSelection}
+          delay={300}
+          inputProps={{
+            spellCheck: false
+          }}
           placeholder="Search for a Github user..."
           // renderMenuItemChildren={(option, props) => (
           //   <span key={option.id}>{option}</span>
