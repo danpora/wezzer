cb495416cd5c4271a40e24157556577fd524f22e
danpora <danpora@post.bgu.ac.il>
2019-03-13 23:17:09 +0200

    Forecast component - make a default view when no location available from browser

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,20 +4,41 @@ import PropTypes from 'prop-types';
 import WeatherIcon from './WeatherIcon';
 
 export default function Forecast(props) {
-  const { className , data } = props;
-  const { weather = [{}], name, sys = {}, main = {} } = data;
+  const isDataNotAvailable = Object.keys(props.data).length === 0;
+  console.log('isDataNotAvailable::', isDataNotAvailable)
   
+  return isDataNotAvailable ? (
+    <NoLocationAvailable className={props.className} />
+  ) : (
+    <WeatherViewer {...props} />
+  );
+}
+
+function NoLocationAvailable(props) {
+  return <div className={props.className}>To begin, please select a location first..</div>;
+}
+
+function WeatherViewer(props) {
+  const { className } = props;
+  const { weather = [{}], name, sys = {}, main = {} } = props.data;
+
   return (
     <div className={className}>
       <div style={{ textAlign: 'center' }}>
         <WeatherIcon code={weather[0].id} />
       </div>
       <div>
-        <div style={{ textAlign: 'center', fontSize: '3em' }}>{weather[0].description}</div>
-        <div style={{ textAlign: 'center', fontSize: '2em' }}>{`${name}, ${sys.country}`}</div>
+        <div style={{ textAlign: 'center', fontSize: '3em' }}>
+          {weather[0].description}
+        </div>
+        <div style={{ textAlign: 'center', fontSize: '2em' }}>{`${name}, ${
+          sys.country
+        }`}</div>
       </div>
       <div>
-        <div style={{ textAlign: 'center', fontSize: '4em' }}>{`${main.temp}°C`}</div>
+        <div style={{ textAlign: 'center', fontSize: '4em' }}>{`${
+          main.temp
+        }°C`}</div>
       </div>
     </div>
   );
@@ -25,7 +46,5 @@ export default function Forecast(props) {
 
 Forecast.propTypes = {
   className: PropTypes.string,
-  data: PropTypes.object
-}
-
-
+  data: PropTypes.object,
+};
