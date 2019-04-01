077903ab7a454854872ee4ed14c10f8c50cac16d
danpora <danpora@post.bgu.ac.il>
2019-04-01 23:47:54 +0300

    change loader component to use wezzer custom loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Loading (props) {
+export default function Loading(props) {
   const { className } = props;
 
   return (
-    <div className={className}>Loading..</div>
+    <div className={className}>
+      <div id="loader-wrapper">
+        <div className="lds-ripple">
+          <div />
+          <div />
+        </div>
+        <span>Wezzer</span>
+      </div>
+    </div>
   );
 }
 
 Loading.propTypes = {
-  className: PropTypes.string
-}
+  className: PropTypes.string,
+};
