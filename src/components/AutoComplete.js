5c425b78b31c93e35796876da1aab8ed68575d65
danpora <danpora@post.bgu.ac.il>
2019-04-09 23:50:55 +0300

    add prop types to autocomplete component

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,18 +1,19 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 
 import * as ApiService from '../services/api';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-export default class AsyncExample extends React.Component {
+export default class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       allowNew: false,
       isLoading: false,
       multiple: false,
-      options: []
+      options: [],
     };
 
     this.handleSearch = this.handleSearch.bind(this);
@@ -22,27 +23,31 @@ export default class AsyncExample extends React.Component {
   makeAndHandleRequest(query) {
     return new Promise((resolve, reject) => {
       ApiService.getCities(query)
-        .then(result => {
-          const cities = result.map(c => ({ name: c.name, location: c.coord, country: c.country }));
+        .then((result) => {
+          const cities = result.map((c) => ({
+            name: c.name,
+            location: c.coord,
+            country: c.country,
+          }));
           resolve(cities);
         })
-        .catch(err => {
+        .catch((err) => {
           reject(err);
-        })
+        });
     });
   }
 
   handleSearch(query) {
     this.setState({ isLoading: true });
-    this.makeAndHandleRequest(query).then(options => {
+    this.makeAndHandleRequest(query).then((options) => {
       this.setState({
         isLoading: false,
-        options
+        options,
       });
     });
   }
 
-  handleSelection (query) {
+  handleSelection(query) {
     if (query.length === 0) return;
 
     const selection = query[0];
@@ -64,7 +69,7 @@ export default class AsyncExample extends React.Component {
           onChange={this.handleSelection}
           delay={300}
           inputProps={{
-            spellCheck: false
+            spellCheck: false,
           }}
           placeholder="Search for a city.."
           renderMenuItemChildren={(option) => (
@@ -75,3 +80,8 @@ export default class AsyncExample extends React.Component {
     );
   }
 }
+
+AutoComplete.propTypes = {
+  handleSelection: PropTypes.func,
+  className: PropTypes.object,
+};
