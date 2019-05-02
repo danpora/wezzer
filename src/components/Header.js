d7dd4a8d3c70bfb13649176e2ad5553fe168e95c
danpora <danpora@post.bgu.ac.il>
2019-05-02 23:24:00 +0300

    add theme button, change header display style to grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,75 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import CloudIcon from '@material-ui/icons/Cloud';
+import BrightIcon from '@material-ui/icons/Brightness7';
+import DarkIcon from '@material-ui/icons/Brightness2';
 
+import Switch from '@material-ui/core/Switch';
 
 const styles = {
   root: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    justifyItems: 'center',
   },
-  headerIcon: {
+  iconContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gridColumnStart: 2,
+  },
+  icon: {
     color: '#6abfea',
     fontSize: '47px',
     transition: 'color 1s',
     '&:hover': {
-      color: '#e6ca57'
-    }
+      color: '#e6ca57',
+    },
   },
   title: {
     display: 'flex',
     alignItems: 'center',
     marginLeft: '10px',
     fontSize: '1.3em',
-    fontWeight: '100'
-  }
+    fontWeight: '100',
+  },
+  switchContainer: {
+    gridColumnStart: 3,
+  },
+  themeIcon: {
+    fontSize: '1em',
+  },
 };
 
 function Header(props) {
   const { classes } = props;
-
   return (
     <div className={classes.root}>
-      <CloudIcon className={classes.headerIcon}/>
+      <Icon classes={classes} />
+      <ThemeButton classes={classes} />
+    </div>
+  );
+}
+
+function Icon({ classes }) {
+  return (
+    <div className={classes.iconContainer}>
+      <CloudIcon className={classes.icon} />
       <span className={classes.title}>Wezzer</span>
+    </div>
+  );
+}
+
+function ThemeButton({ classes, isDarkTheme, handleThemeToggle }) {
+  return (
+    <div className={classes.switchContainer}>
+      <BrightIcon className={classes.themeIcon} />
+      <Switch
+        checked={isDarkTheme}
+        onChange={handleThemeToggle}
+        value="dark"
+        color="primary"
+      />
+      <DarkIcon className={classes.themeIcon} />
     </div>
   );
 }
